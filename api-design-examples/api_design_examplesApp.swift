e5e66b3bdd844787f9aa96c01624255103e1b7e3
Eslam Nasser <eslamoz.nasser@gmail.com>
2025-06-02 07:56:36 +0200

    Initial Commit

---
+++ api-design-examples/api_design_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_design_examplesApp.swift
+//  api-design-examples
+//
+//  Created by Eslam Nasser on 2025-06-02.
+//
+
+import SwiftUI
+
+@main
+struct api_design_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
